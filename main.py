b9002bf4b64b8a96ab4f5753444d36246dc0a90c
“Brandon <cassias.long0r@icloud.com>
2022-10-06 21:08:55 -0700

    working weather build

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ WIN_SIZE = "200x100"
 def close_window():
     window.destroy()
 
-def check_weather(stuff):
-    # Check the weather
-    print(stuff)
-
+def get_weather(zip):
+    # get the weather for the given zip code using the API
+    import requests
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip}&appid=49da89b32f4c7a4fc739dc2ba9f278d0"
+    response = requests.get(url)
+    # set the text of the label to the weather temp
+    try:
+        temp = response.json()["main"]["temp"]
+        # convert the temp from Kelvin to Fahrenheit
+        temp = (temp - 273.15) * 9/5 + 32
+        # round the temp to whole number
+        temp = round(temp)
+        # set the text of the label to the temp
+        label["text"] = str(temp) + "°F"
+    except:
+        label["text"] = "Error"
 ### ? Main Program Entry Point ###
 
 window = tk.Tk()
@@ -31,8 +43,8 @@ label.pack()
 # * clicking enter in the txt box will call this function
 text = tk.Entry(window, width=20)
 text.focus_set()
-# bind the enter key to the check_weather function
-window.bind('<Return>', lambda event: check_weather(text.get()))
+# bind the enter key to the get_weather function
+window.bind('<Return>', lambda event: get_weather(text.get()))
 text.pack()
 
 
